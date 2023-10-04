b3fffcfd6804d8f02b3ceec8042fe551d4ce3728
Vadzim_Kavalkou <vadzim_kavalkou@epam.com>
2023-10-04 11:06:13 +0300

    CI fixes

--- src/test/java/by/vk/catalog/CatalogServiceApplicationTests.java
+++ src/test/java/by/vk/catalog/CatalogServiceApplicationTests.java
@@ -1,13 +1,18 @@
 package by.vk.catalog;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 class CatalogServiceApplicationTests {
 
-  @Test
-  void contextLoads() {
+  @Bean
+  @ServiceConnection
+  ElasticsearchContainer elasticsearchContainer() {
+    return new ElasticsearchContainer(
+        DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch:7.17.10"));
   }
-
 }
